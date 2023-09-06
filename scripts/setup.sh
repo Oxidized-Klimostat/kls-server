a04e10e3b27ee24abcb835a934563b70396edb22
Florentin Schäfer <florentin.schaefer@gmail.com>
2023-09-06 15:35:11 +0200

    Replace env vairables with Docker secrets

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+PGPASSWORD=$(cat /run/secrets/postgres_password) \
 psql postgres -U postgres \
-    --set=auth_pass="$AUTH_PASS" \
+    --set=auth_pass="$(cat /run/secrets/auth_pass)" \
     -f /setup.sql
