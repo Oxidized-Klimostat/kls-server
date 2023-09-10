6a5a9103f2e8f60d04cb3d75ffca469e8ba8bf4a
Florentin Schäfer <florentin.schaefer@gmail.com>
2023-09-10 21:54:23 +0200

    Add add_station and auth_station functions

--- setup.sql
+++ setup.sql
@@ -5,11 +5,56 @@ CREATE EXTENSION pgjwt CASCADE;
 
 ALTER DATABASE postgres SET "kls.jwt_secret" TO :'jwt_secret';
 
-CREATE TYPE jwt_token AS (
-  token text
+
+CREATE TABLE IF NOT EXISTS stations (
+  id serial primary key,
+  token text not null,
+  description text not null,
+  created timestamptz not null
 );
 
-CREATE OR REPLACE FUNCTION jwt_test() RETURNS public.jwt_token AS $$
+
+CREATE OR REPLACE FUNCTION add_station(description text) RETURNS RECORD AS $$
+DECLARE
+  uuid text := gen_random_uuid();
+  ret RECORD;
+  station_id integer;
+BEGIN
+  INSERT INTO stations (token, description, created) VALUES
+    (crypt(uuid, gen_salt('bf')), description, now()) RETURNING id INTO station_id;
+
+  SELECT uuid, station_id INTO ret;
+  RETURN ret;
+END
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION auth_station(check_id integer, check_token text) RETURNS text AS $$
+DECLARE
+  stored_token text;
+  jwt_token text;
+BEGIN
+  SELECT token FROM stations WHERE id = check_id INTO stored_token;
+  IF NOT stored_token = crypt(check_token, stored_token) THEN
+    raise invalid_password using message = 'invalid station_id or token';
+  END IF;
+
+  SELECT public.sign(
+    row_to_json(r), current_setting('kls.jwt_secret')
+  ) AS token
+  FROM (
+    SELECT
+      'station'::text as role,
+      extract(epoch from now())::integer + 30 AS exp
+  ) r
+  INTO jwt_token;
+  RETURN jwt_token;
+END
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION jwt_test() RETURNS text AS $$
   SELECT public.sign(
     row_to_json(r), current_setting('kls.jwt_secret')
   ) AS token
@@ -20,17 +65,8 @@ CREATE OR REPLACE FUNCTION jwt_test() RETURNS public.jwt_token AS $$
   ) r;
 $$ LANGUAGE sql;
 
-CREATE TABLE todos (
-    id SERIAL PRIMARY KEY,
-    done BOOLEAN NOT NULL DEFAULT false,
-    task TEXT NOT NULL,
-    due TIMESTAMPTZ
-);
-INSERT INTO todos (task) VALUES
-    ('finish tutorial 0'), ('pat self on back');
 
 CREATE ROLE anon nologin;
-GRANT SELECT ON todos TO anon;
 
 CREATE ROLE authenticator noinherit LOGIN PASSWORD :'auth_pass';
 GRANT anon TO authenticator;
